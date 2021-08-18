bb883a07452ec0e530d8ffc97ff0a6b95d8e20d3
Damola Oladeji <darmhoo@hotmail.com>
2021-08-18 16:09:53 +0100

    remmoved ui

--- routes/api.php
+++ routes/api.php
@@ -15,38 +15,9 @@ Route::post('/login', 'AuthController@login');
 Route::post('/password/reset', 'AuthController@sendResetLinkEmail');
 Route::put('/password/reset', 'AuthController@reset');
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/branches', 'BranchController@allBranches');
-    Route::post('/customer/autocomplete', 'CustomerController@autocompleteSearch');
-    Route::get('/customer/lookup/{customer}', 'CustomerController@customerLookup');
-    Route::post('/customer/{customer}', 'CustomerController@update');
     Route::get('/user/getBranchUsers', 'UserController@getBranchUsers');
     Route::Resources([
-        'log' => 'LogController',
         'user' => 'UserController',
-        'bank' => 'BankController',
-        'state' => 'StateController',
-        'brand' => 'BrandController',
-        'branch' => 'BranchController',
-        'address' => 'AddressController',
-        'message' => 'MessageController',
-        'caution' => 'CautionController',
-        'product' => 'ProductController',
-        'customer' => 'CustomerController',
-        'category' => 'CategoryController',
-        'document' => 'DocumentController',
-        'supplier' => 'SupplierController',
-        'reminder' => 'ReminderController',
-        'attendance' => 'AttendanceController',
-        'promise_call' => 'PromiseCallController',
-        'verification' => 'VerificationController',
-        'work_guarantor' => 'WorkGuarantorController',
-        'processing_fee' => 'ProcessingFeeController',
-        'personal_guarantor' => 'PersonalGuarantorController',
-        'dsa_daily_registration' => 'DsaDailyRegistrationController',
-        'update_customer_manager' => 'CustomerManagementHistoryController',
-        'repayment' => 'RepaymentController',
-        'payment_method' => 'PaymentMethodController',
-        'renewal-list' => 'RenewalListController',
     ]);
     /*------*/
     Route::get('/users/list_type/{type}', 'UserController@getListForTypeahead');
@@ -57,12 +28,7 @@ Route::group(['middleware' => ['auth:api']], function () {
     Route::get('/employee/{id}/edit', 'AuthController@edit');
     Route::post('/employee/{id}/update', 'AuthController@update');
     Route::get('/reset-password/{id}', 'AuthController@resetPassword');
-    Route::get('/orders/user/{id}', 'OrderController@ordersByUser');
-    Route::get('/orders/direct-debit-sales', 'OrderController@directDebitSales');
-    Route::post('/report', 'ReportController@generateReport');
-    Route::post('/report/daily', 'ReportController@getRegistrationReport');
-    Route::get('/renewal-list/status/{status}', 'RenewalListController@list');
-    Route::get('/renewal-list-status', 'RenewalListStatusController@index');
+
 });
 
-    Route::post('/credit-check', 'CreditCheckController@check');
+
